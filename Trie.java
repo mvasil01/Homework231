048ded58c63129c1b6eb797b74a65417ae02762a
Markos Vasili <mvasil01@ucy.ac.cy>
2025-11-03 14:08:52 +0200

    first upload

---
+++ Trie.java
@@ -0,0 +1,11 @@
+private class TrieNode {
+private TrieNode[] children; // πίνακας 26 θέσεων για a-z
+private boolean isEndOfWord; // true αν εδώ τελειώνει λέξη
+public TrieNode() { /* Υλοποίηση */ }
+}
+public class Trie {
+private TrieNode root;
+public Trie() { /* Υλοποίηση */ }
+public search(String word) { /* Υλοποίηση */ }
+public insert(String word) { /* Υλοποίηση */ }
+}
