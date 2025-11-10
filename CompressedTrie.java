ee673a4ce585c64aef974f599545cb0b190241e8
Markos Vasili <mvasil01@ucy.ac.cy>
2025-11-10 19:55:16 +0200

    CompressedTrie completed

--- CompressedTrie.java
+++ CompressedTrie.java
@@ -6,30 +6,96 @@ public class CompressedTrie {
     }
 
     public void insert(String word){
-        if(edgeList == null){
-            edgeList.insert(edge);
+        if(word == null){
             return;
         }
-        char c = edge.label.charAt(0);
-        Edge current = edgeList.getEdge(c);
-        if(current == null){
-            edgeList.insert(edge);
-            return;
+        insertAt(root, word);
+    }
+
+    private void insertHelper(CompressedTrieNode current, String word){
+        if(word.isEmpty()){
+            current.isEndOfWord = true;
         }
-        
+
+        Edge edge = current.getEdgeByFirstChar(word.charAt(0));
+        if(edge == null){
+            CompressedTrieNode child = new CompressedTrieNode();
+            child.isEndOfWord = true;
+            current.insertEdge(new Edge(word, child));
+        }
+
+        String label = edge.label;
+        int prefixLength = commonPrefixLength(word, label);
+
+        if(prefixLength == label.length() && prefixLength == word.length()){
+            edge.child.isEndOfWord = true;
+        }
+        else if(prefixLength == label.length() && prefixLength < word.length()){
+            insertHelper(edge.child, word.substring(prefixLength));
+        }
+        else if(prefixLength < label.length() && prefixLength == word.length()){
+            CompressedTrieNode oldChild = edge.child;
+            CompressedTrieNode mid = new CompressedTrieNode();
+            mid.isEndOfWord = true;
+            String remainder = label.substring(prefixLength);
+            edge.label = word;
+            edge.child = mid;
+            mid.insertEdge(new Edge(remainder, oldChild));
+        }
+        else if (prefixLength < word.length() && prefixLength < label.length()){
+            CompressedTrieNode oldChild = edge.child;
+            CompressedTrieNode mid = new CompressedTrieNode();
+            String prefix = label.substring(0, prefixLength);
+            String remainderWord = word.substring(prefixLength);
+            String remainderLabel = label.substring(prefixLength);
+            edge.label = prefix;
+            edge.child = mid;
+            mid.insertEdge(new Edge(remainderLabel, oldChild));
+            CompressedTrieNode newChild = new CompressedTrieNode();
+            newChild.isEndOfWord = true;
+            mid.insertEdge(new Edge(remainderWord, newChild));   
+        }
+        else{
+            CompressedTrieNode child = new CompressedTrieNode();
+            child.isEndOfWord = true;
+            current.insertEdge(new Edge(word, child));
+        }
+    }
+
+    private static int commonPrefixLength(String a, String b){
+        int n = Math.min(a.length(), b.length());
         int cnt = 0;
-        String rest;
-        for(int i = 0; i < current.label.length(); i++){
-            if(current.label.charAt(i) != edge.label.charAt(i)){
-                break;
-            }
+        while(cnt < n && a.charAt(cnt) == b.charAt(cnt)){
             cnt++;
         }
-        rest = current.label.substring(cnt - 1);
-        CompressedTrieNode newChild = new CompressedTrieNode();
-        newChild.edgeList.insert(new Edge(rest, current.child));
-        rest = edge.label.substring()
-        current.label = current.label.substring(0, cnt - 1);
-        current.child = newChild;
+
+        return cnt;
+    }
+
+    public boolean search(String word){
+        if(word == null){
+            return false;
+        }
+        return searchHelper(root, word);
+    }
+
+    public boolean searchHelper(CompressedTrieNode current, String word){
+        if(word.isEmpty()){
+            return false;
+        }
+        Edge edge = current.getEdgeByFirstChar(word.charAt(0));
+        if(edge == null){
+            return false;
+        }
+
+        String label = edge.label;
+        int prefixLength = commonPrefixLength(label, word);
+        if(prefixLength == label.length() && prefixLength == word.length()){
+            return edge.child.isEndOfWord;
+        }
+        if (prefixLength == label.length() && prefixLength < word.length()) {
+            return searchHelper(edge.child, word.substring(prefixLength));
+        }
+        return false;
     }
 }
