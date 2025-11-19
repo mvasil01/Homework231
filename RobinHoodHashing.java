dfcf3dd903c1698358a3d89f6aa88cc4a22781d5
Markos Vasili <mvasil01@ucy.ac.cy>
2025-11-19 18:57:31 +0200

    rehash finished and fixes in all methods

--- RobinHoodHashing.java
+++ RobinHoodHashing.java
@@ -14,12 +14,12 @@ public class RobinHoodHashing{
         maxProbeLength = 0;
     }
 
-    private static int hashFunction(String label, Edge hashTable[]){
-        return (label.charAt(0) - 'a') % hashTable.length;
+    private int hashFunction(String label){
+        return (label.charAt(0) - 'a') % capacity;
     }
 
     private int probeLength(int pos){
-        int expected = hashFunction(hashTable[pos].label, hashTable);
+        int expected = hashFunction(hashTable[pos].label);
 
         if(pos < expected){
             return capacity - expected + pos;
@@ -30,42 +30,64 @@ public class RobinHoodHashing{
     }
 
     public void insert(Edge edge){
-        size++;
-        if(needsRehash()){
-            this.rehash();
+        if(edge == null || edge.label == null){
+            return;
         }
 
-        int pos = hashFunction(edge.label, hashTable);
-        if(!hashTable[pos].occupied){
-            hashTable[pos] = edge;
-        }
-        else{
-            int newProbeLength = 0;
-            while(newProbeLength < probeLength(pos)){
-                Edge conflict = hashTable[pos];
-                newProbeLength = probeLength(pos);
-                hashTable[pos] = edge;
-                if(pos + 1 < capacity){
-                    pos++;
-                }
-                else{
-                    pos = 0;
-                }
-                edge = conflict;
-                if(newProbeLength > maxProbeLength){
-                    maxProbeLength = newProbeLength;
-                }
-            }
-            hashTable[pos] = edge;
+        if((size + 1.0) / capacity > 0.9){
+            rehash();
         }
     
+        insertNoRehash(edge);
+    }
+
+    private void insertNoRehash(Edge edge) {
+        int home = hashFunction(edge.label);
+        int idx = home;
+        int probeNew = 0;
+
+        while (true) {
+            Edge current = hashTable[idx];
+
+            if (current == null || !current.occupied) {
+                edge.occupied = true;
+                hashTable[idx] = edge;
+                size++;
+                if (probeNew > maxProbeLength) {
+                    maxProbeLength = probeNew;
+                }
+                return;
+            }
+
+            if (current.label.equals(edge.label)) {
+                return;
+            }
+
+            int homeCurrent = hashFunction(current.label);
+            int probeCurrent = (idx - homeCurrent + capacity) % capacity;
+
+            if (probeNew > probeCurrent) {
+                Edge temp = hashTable[idx];
+                hashTable[idx] = edge;
+                edge = temp;
+                probeNew = probeCurrent;
+            }
+
+            idx = (idx + 1) % capacity;
+            probeNew++;
+        }
     }
     
     public boolean getEdge(String label){
-        int pos = hashFunction(label, hashTable);
-
-        for (int i = pos; i <= maxProbeLength; i++){
-            if (hashTable[i].label.equals(label)){
+        int pos = hashFunction(label);
+        int index;
+        for(int offset = 0; offset <= maxProbeLength; offset++){
+            index = (pos + offset) % capacity;
+            Edge e = hashTable[index];
+            if(e == null){
+                return false;
+            }
+            if(e.occupied && e.label.equals(label)){
                 return true;
             }
         }
@@ -78,17 +100,28 @@ public class RobinHoodHashing{
     }
 
     private void rehash(){
-        
-        int newSize = 0;
-        while (newSize < PRIMES.length){
-            if (capacity == PRIMES[newSize++]){
-                break;
-            }
-        
+        int index = 0;
+        while(index < PRIMES.length && PRIMES[index] != capacity){
+            index++;
         }
 
-        Edge newHashTable[] = new Edge[PRIMES[newSize]];
+        if(index + 1 >= PRIMES.length){
+            return;         // no larger Prime number
+        }
 
+        int newCapacity = PRIMES[index + 1];
+        Edge[] oldTable = hashTable;
+
+        hashTable = new Edge[newCapacity];
+        capacity = newCapacity;
+        size = 0;
+        maxProbeLength = 0;
+
+        for(Edge e : oldTable){
+            if(e != null && e.occupied){
+                insertNoRehash(e);
+            }
+        }
         
     }
 }
